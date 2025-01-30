6c7b77f815d7d8500ac0c09c05b8a9571ad7f266
Tayo <tyson.natanael@hotmail.com>
2025-01-30 02:07:14 +0000

    fiz a estrutura da galeria e adicionei algumas fotos

--- src/app/components/navbar/navigation.tsx
+++ src/app/components/navbar/navigation.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import NavbarBrand from 'react-bootstrap/NavbarBrand'
-import NavbarToggle from 'react-bootstrap/NavbarToggle'
-import NavbarCollapse from 'react-bootstrap/NavbarCollapse'
-import NavLink from 'react-bootstrap/NavLink'
-
+import NavbarBrand from "react-bootstrap/NavbarBrand";
+import NavbarToggle from "react-bootstrap/NavbarToggle";
+import NavbarCollapse from "react-bootstrap/NavbarCollapse";
+import NavLink from "react-bootstrap/NavLink";
+import "../../../styles/nav.css"
 
 const Navigation: React.FC = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
-        <NavbarBrand href="#">Naeole <span style={{ color: "rgb(237, 190, 119)" }}>&</span> Mario</NavbarBrand>
+        <NavbarBrand href="/">
+          Naeole <span style={{ color: "rgb(237, 190, 119)" }}>&</span> Mario
+        </NavbarBrand>
         <NavbarToggle aria-controls="navbarScroll" />
         <NavbarCollapse id="navbarScroll">
           <Nav
@@ -23,21 +23,14 @@ const Navigation: React.FC = () => {
             style={{ maxHeight: "100px" }}
             navbarScroll
           >
-            <NavLink href="/">Home</NavLink>
-            <NavLink href="#action2">Link</NavLink>
-            <NavLink href="#" disabled>
-              Link
-            </NavLink>
+            <NavLink href="/">Início</NavLink>
+            <NavLink href="/galeria">Galeria de fotos</NavLink>
+            <NavLink href="/formulario">Confirme a sua presença </NavLink>
+            <NavLink href="/info">Informações do casamento</NavLink>
           </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
+          <Nav className="d-flex">
+            <Button href="admin" variant="outline-primary" id="btn-1" >Admin</Button>
+          </Nav>
         </NavbarCollapse>
       </Container>
     </Navbar>
