2c90266313a33a8a1f7af3bd66fb85644d8505c1
Tayo <tyson.natanael@hotmail.com>
2025-02-03 11:55:23 +0000

    update 2

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,12 +1,10 @@
-// Import the functions you need from the SDKs you need
-import {  FirebaseApp } from "firebase/app";
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
 import { getAuth, Auth } from "firebase/auth";
 import { getFirestore, Firestore, collection, CollectionReference } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+// Configuração do Firebase
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: "AIzaSyB1RHZvfewrrIsH-ApsM2qrfYWEPRn-4do",
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
@@ -14,21 +12,10 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase only on the client side
-let app: FirebaseApp | undefined;
-let auth: Auth | undefined;
-let db: Firestore | undefined;
-let convidadosRef: CollectionReference | undefined;
-
-if (typeof window !== "undefined") {
-  // Code is running in the browser
-  app = initializeApp(firebaseConfig);
-  auth = getAuth(app);
-  db = getFirestore(app);
-  convidadosRef = collection(db, "convidados");
-} else {
-  // Skip Firebase initialization on the server side
-  console.warn("Firebase is running on the server. Initialization was skipped.");
-}
+// Evita inicializar o Firebase mais de uma vez
+const app: FirebaseApp = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const auth: Auth = getAuth(app);
+const db: Firestore = getFirestore(app);
+const convidadosRef: CollectionReference = collection(db, "convidados");
 
 export { auth, db, convidadosRef, app };
